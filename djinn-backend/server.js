ad5cef1662d3867f0d122c77f4dc23ef855268a5
Aryan Binazir <65728619+aryan-binazir@users.noreply.github.com>
2022-09-15 18:58:55 -0400

    Update server.js
    Co-Authored-By: aashish-b <87250229+aashish-b@users.noreply.github.com>

--- djinn-backend/server.js
+++ djinn-backend/server.js
@@ -15,7 +15,7 @@ console.log(Request, doc);
 // dotenv.config()
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL);
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to MongoDB'));
