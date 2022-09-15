0a69dd77f6a5e31ef13775c268b1070eae6f67c4
Aryan Binazir <65728619+aryan-binazir@users.noreply.github.com>
2022-09-14 20:35:41 -0400

    Add SQL arr format

--- djinn-backend/schema.sql
+++ djinn-backend/schema.sql
@@ -1,17 +1,15 @@
-DROP TABLE bins
+DROP TABLE bins;
 
 CREATE TABLE bins (
 binID SERIAL PRIMARY KEY,
 binKey VARCHAR(50) NOT NULL,
 createdTime timestamp,
 endPoint VARCHAR(50),
-supported VARCHAR(50),
 last timestamp,
-count int,
-status VARCHAR(50)
+count numeric
 );
 
 
 -- Sample Data
-INSERT INTO bins (binKey, createdTime, endPoint, supported, last, status)
-    VALUES ('abc123', '2011-10-19 10:23:54', 'www.test.com', 'DNS,HTTP', '2011-10-19 10:23:54', 'active');
+INSERT INTO bins (binKey, createdTime, endPoint, last, count)
+    VALUES ('abc123', '2011-10-19 10:23:54', 'www.test.com', '2011-10-19 10:23:54', 1);
--- djinn-backend/server.js
+++ djinn-backend/server.js
@@ -5,9 +5,14 @@ const mongoose = require('mongoose');
 // const mongoDb = require("Request");
 const Request = require('./binDb.js');
 const hash = require('object-hash');
+const { pool } = require("./relationalDb.js");
+const bodyParser = require('body-parser');
+
 
 const doc = new Request();
 console.log(Request, doc);
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // connecting to mongo: mongoose.connect
@@ -34,10 +39,32 @@ else return {status: 400, error: malformed request}
 app.post('/bin', (req, res) => {
   console.log('got here')
   let newBinKey = makeHash();
+  let webhookURL = 'http://' + makeHash() + '.request-djinn.com'
+  // console.log(webhookURL)
+  let sqlArr = parseReqNewBin(req, newBinKey, webhookURL);
+  console.log(sqlArr);
   // create psql record with binKey and subdomain
   res.status(201).send({ status: 201, binKey: newBinKey });
+  
   // res.status(404).send('Sorry, we cannot find that!')
 });
+// console.log(JSON.stringify(request.body, null, 2));
+
+
+// {binKey: binKey, createdTime: createdTime, }
+
+// pool.connect((err, client, release) => {
+//   if (err) {
+//     return console.error('400', err.stack)
+//   }//(binKey, createdTime, endPoint, supported, last, status)
+//   client.query('INSERT into bins ()', (err, result) => {
+//     release()
+//     if (err) {
+//       return console.error('Error executing query', err.stack)
+//     }
+//     console.log(result.rows)
+//   })
+// })
 
 // GET TO ‘/bin/:binId/requests’	{status: 200, requests: array of json objects}	
 // {status: 400, error: malformed request} 	array of json objs
@@ -94,6 +121,48 @@ function getSubdomain(headersObj) {
 function makeHash() {
   return hash([Math.random(), Math.random()]);
 }
+
+//binKey (from makeHash),
+// createdTime (e.g. 2022-09-12 23:14:16.244501)
+// endPoint (e.g. asdf2345.request-djinn.com)
+//supported (e.g. DNS, HTTP)
+//last (e.g. 2022-09-13 23:01:28.505521),
+//status
+//Count (e.g. 8)
+//Status (e.g. active)
+
+/*
+function to parse request and return array in format below
+- [binKey, createdTime, endPoint(webhook url), last]
+- http://iqwerudfgbqwirougrbvwe.request-djinn.com
+*/
+
+function parseReqNewBin(request, binKey, webhookURL) {
+  // console.log(request.headers) // check for .body
+  // console.log(request.headers.host)
+  // console.log(request.body)
+  // console.log(request)
+  const timestamp = getTimeStamp();
+  return [binKey, timestamp, webhookURL, timestamp, 0];
+}
+
+function getTimeStamp() {
+  return new Date(Date.now()).toISOString();
+}
+
+async function insertData() {
+  try {
+    const res = await pool.query(
+      //  const test = [name, color]
+      "INSERT INTO bins (binKey) VALUES ('abc123')",
+      // "INSERT INTO bins (name, color) VALUES ($1, $2)"
+    );
+    console.log(`Added a row`);
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 
 // console.log(getSubdomain(testObj)) // 'jordansbin'
 
