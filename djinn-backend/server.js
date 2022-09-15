c8857fca59457d52a98ef69b02b0bc0160725dca
Aryan Binazir <65728619+aryan-binazir@users.noreply.github.com>
2022-09-15 18:57:17 -0400

    Update server.js
    Co-Authored-By: aashish-b <87250229+aashish-b@users.noreply.github.com>

--- djinn-backend/server.js
+++ djinn-backend/server.js
@@ -15,7 +15,7 @@ console.log(Request, doc);
 // dotenv.config()
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.4')//process.env.MONGODB_URL);
+mongoose.connect(process.env.MONGODB_URL);
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to MongoDB'));
