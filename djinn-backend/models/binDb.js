4f0de9387d1345ece9d60eb31abc0a23089f8cb5
aashish-b <87250229+aashish-b@users.noreply.github.com>
2022-09-18 11:14:29 -0400

    Update binDb.js

--- djinn-backend/models/binDb.js
+++ djinn-backend/models/binDb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const requestSchema = new Schema({
-  requestId: { // changed from contentId.
+  requestId: { 
     type: String,
     required: true,
     unique: true
@@ -14,62 +14,12 @@ const requestSchema = new Schema({
   headers: {
     type: Array,
     required: true
-  }, // url needs to go into postgres to find binkey, then get requests from mongo.
+  }, 
  body: {
-    type: Object, // is this JSON or just string? // Aryan -> (I would say JSON)
+    type: Object, 
   }
 });
 
-
-
-// Create model
 const Request = mongoose.model('requests', requestSchema);
 
 module.exports = Request;
-
-
-
-
-
-
-
-// PROBABLY TO BE DELETED 
-// could all of the required ones go into a requiredHeaders attribute so we just reduce the size?
-// binKey: {
-//   type: String,
-//   required: true,
-//   unique: true
-// },
-// host: {
-//   type: String,
-//   required: true
-// },
-// fromIp: {
-//   type: String,
-//   required: true
-// },
-// requestMethod: {
-//   type: String,
-//   required: true
-// },
-// connection: {
-//   type: String,
-//   required: true
-// },
-// userAgent: {
-//   type: String,
-//   required: true
-// },
-// accept: {
-//   type: String,
-//   required: true
-// },
-// createdTime: {
-//   type: Timestamp,
-//   default: Date.now,
-//   required: true
-// },
-// contentType: {
-//   type: String,
-//   required: true
-// },
